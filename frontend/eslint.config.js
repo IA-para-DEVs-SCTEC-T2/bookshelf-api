126519e398ee8a8a3bccfb8f3e10744768c04a8e
wanderson <wandersonsouza.info@gmail.com>
2026-05-18 12:07:33 -0300

    fix: sync package lock files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,27 +1,34 @@
+import react from 'eslint-plugin-react';
+
 export default [
- {
-   files: ['src/**/*.js', 'src/**/*.jsx'],
-   languageOptions: {
-     ecmaVersion: 'latest',
-     sourceType: 'module',
-     parserOptions: {
-       ecmaFeatures: {
-         jsx: true
-       }
-     },
-     globals: {
-       document: 'readonly',
-       window: 'readonly',
-       console: 'readonly',
-       describe: 'readonly',
-       test: 'readonly',
-       expect: 'readonly'
-     }
-   },
-   rules: {
-     'no-unused-vars': 'error',
-     semi: ['error', 'always'],
-     quotes: ['error', 'single']
-   }
- }
+  {
+    files: ['src/**/*.js', 'src/**/*.jsx'],
+    plugins: {
+      react
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        document: 'readonly',
+        window: 'readonly',
+        console: 'readonly',
+        describe: 'readonly',
+        test: 'readonly',
+        expect: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react/jsx-uses-react': 'off',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single']
+    }
+  }
 ];
