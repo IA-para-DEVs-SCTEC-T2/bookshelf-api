4be85fda65d6271e2d775ef44571e40c3d2e074a
jacson <jacson@centrium.com.br>
2026-05-19 21:40:11 -0300

    docs: OpenAPI/Swagger

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
  console.log(`BookShelf API running on port ${PORT}`);
